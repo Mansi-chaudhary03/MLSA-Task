1a73b597e953de9611f9aa63a0b23ebcf240c29c
Mansi <129415531+Mansi-chaudhary03@users.noreply.github.com>
2023-12-31 08:45:38 +0530

    add all changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/images1.jpg";
+import image from "../images/images2.jpg";
 
 const imageAltText = "Mansi Chaudhary : Tech enthusiast and aspiring developer from India, diving into the world of coding";
 
